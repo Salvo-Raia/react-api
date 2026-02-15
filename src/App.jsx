1cebdaa8c9edf93be2503ddf831cd3d1ebe912ac
Salvo-Raia <salvo.raia@outlook.it>
2026-02-15 16:03:27 +0100

    Simple map of actors name list

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,15 @@ const actorsEndpoint = "https://lanciweb.github.io/demo/api/actors/"
 export default function App() {
   const [actors, setActors] = useState ([]);
   useEffect(() => {axios.get(actorsEndpoint)
-                        .then((res) => {console.table(res.data);
-    })}, 
+                        .then((res) => {setActors(res.data)})}, 
     []);
   
   return ( 
     <div>
       <ul>
-
+        {actors.map ((actor) => (
+          <li key={actor.id}>{actor.name}</li>
+        ))}
       </ul>
     </div>
   )
