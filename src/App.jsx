006179617d5f6da5a13a24bc0cbe25e9c538a9c5
Salvo-Raia <salvo.raia@outlook.it>
2026-02-15 18:28:30 +0100

    Add both axios calls for one render

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,22 @@ const actorsEndpoint = "https://lanciweb.github.io/demo/api/actors/"
 const actressesEndpoint = "https://lanciweb.github.io/demo/api/actresses/"
 
 export default function App() {
-  const [actors, setActors] = useState ([]);
-  useEffect(() => {axios.get(actorsEndpoint)
-                        .then((res) => {setActors(res.data)})}, 
-    []);
-
-  const [actresses, setActresses] = useState ([]); 
-  useEffect(() => {axios.get(actressesEndpoint)
-                        .then((res) => {setActresses(res.data)})}, 
-    []);
+  const [allActors, setAllActors] = useState ([]);
+  
+  useEffect(() => {
+  Promise.all([
+    axios.get(actorsEndpoint),
+    axios.get(actressesEndpoint)
+  ])
+  .then(([actorsRes, actressesRes]) => {
+    const actorsCombined = [
+      ...actorsRes.data,
+      ...actressesRes.data
+    ];
+    setAllActors(actorsCombined); 
+  })
+  .catch(err => console.error(err));
+}, []);
 
   return ( 
     <>
@@ -21,39 +28,20 @@ export default function App() {
       <h1>Hollywood Most Loved</h1>
     </header>
     <div className='container'>
-      <div className='actors-list row row-cols-1 g-2 my-2'>
-        <h2>Actors List</h2>
-        {actors.map ((actor) => (
-          <div key={actor.id} className='col border border-secondary rounded p-0'>
+      <div className='actors-list row row-cols-2 g-3 my-2'>
+
+        {allActors.map ((actor, index) => (
+          <div key={index} className='col border border-secondary rounded p-0'>
             <div className='actor-card d-flex'>
             <img src={actor.image} alt={actor.name} title={actor.name}   onError={(e) => {
-                 e.target.src = "https://via.placeholder.com/150x220?text=No+Image"}}/>
+                 e.target.src = "https://placehold.co/150x220?text=No+Image&bg=e9ecef&fg=6c757d"}}/>
             <div className='actor-card-info text-start p-2'>
               <h2 className='h3 mb-3'>{actor.name}</h2>
               <p className='m-0'><strong>Year of birth:</strong> {actor.birth_year}</p>
               <p className='m-0'><strong>Nationality:</strong> {actor.nationality}</p>
-              <p className='m-0'><strong>Awards:</strong> {actor.awards.join(", ")}</p>
-              <p className='m-0'><strong>Famous Appearances: </strong> <i>{actor.known_for.join(", ")}</i></p>
+              <p className='m-0'><strong>Awards:</strong> {actor.awards}</p>
+              <p className='m-0'><strong>Famous Appearances: </strong> <i>{actor.known_for}</i></p>
               <p className='mt-3'>{actor.biography}</p>
-            </div>
-          </div>
-            </div>
-        ))}
-      </div>
-      <div className='actresses-list row row-cols-1 g-2 my-2'>
-        <h2>Actresses List</h2>
-        {actresses.map ((actress) => (
-          <div key={actress.id} className='col border border-secondary rounded p-0'>
-            <div className='actress-card d-flex'>
-            <img src={actress.image} alt={actress.name} title={actress.name}/>
-            <div className='actress-card-info text-start p-2'>
-              <h2 className='h3 mb-3'>{actress.name}</h2>
-              <p className='m-0'><strong>Year of birth:</strong> {actress.birth_year}</p>
-              <p className='m-0'><strong>Nationality:</strong> {actress.nationality}</p>
-              <p className='m-0'><strong>Awards:</strong> {actress.awards}</p>
-              <p className='m-0'><strong>Famous Appearances: </strong> <i>{actress.most_famous_movies.join(", ")}</i></p>
-              
-              <p className='mt-3'>{actress.biography}</p>
             </div>
           </div>
             </div>
