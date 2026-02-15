d7a2a606be0833f49fd7015e720ad2035fb7a6fe
Salvo-Raia <salvo.raia@outlook.it>
2026-02-15 17:10:06 +0100

    Add Actress cards

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,26 @@ import axios from 'axios'
 import { useEffect, useState } from 'react'
 import './App.css'
 const actorsEndpoint = "https://lanciweb.github.io/demo/api/actors/"
+const actressesEndpoint = "https://lanciweb.github.io/demo/api/actresses/"
 
 export default function App() {
   const [actors, setActors] = useState ([]);
   useEffect(() => {axios.get(actorsEndpoint)
                         .then((res) => {setActors(res.data)})}, 
     []);
-  
+
+  const [actresses, setActresses] = useState ([]); 
+  useEffect(() => {axios.get(actressesEndpoint)
+                        .then((res) => {setActresses(res.data)})}, 
+    []);
+
   return ( 
     <>
     <header>
       <h1>Actor List</h1>
     </header>
     <div className='container'>
-      <div className='row row-cols-2 g-3'>
+      <div className='actors-list row row-cols-1 g-3'>
         {actors.map ((actor) => (
           <div key={actor.id} className='col border border-secondary rounded p-0'>
             <div className='actor-card d-flex'>
@@ -31,7 +37,23 @@ export default function App() {
           </div>
             </div>
         ))}
-        </div>
+
+        {actresses.map ((actress) => (
+          <div key={actress.id} className='col border border-secondary rounded p-0'>
+            <div className='actress-card d-flex'>
+            <img src={actress.image} alt={actress.name} title={actress.name}/>
+            <div className='actress-card-info text-start p-2'>
+              <h2 className='h3 mb-3'>{actress.name}</h2>
+              <p className='m-0'><strong>Year of birth:</strong> {actress.birth_year}</p>
+              <p className='m-0'><strong>Nationality:</strong> {actress.nationality}</p>
+              <p className='m-0'><strong>Awards:</strong> {actress.awards}</p>
+              <p className='m-0'><strong>Famous Appearances: </strong> <i>{actress.known_for}</i></p>
+              <p className='mt-3'>{actress.biography}</p>
+            </div>
+          </div>
+            </div>
+        ))}
+      </div>
     </div>
     </>
   )
