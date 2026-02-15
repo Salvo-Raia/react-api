f104c13bb004c603babdfed7c86a209d815716f3
Salvo-Raia <salvo.raia@outlook.it>
2026-02-15 16:00:28 +0100

    Milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import axios from 'axios'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
-
+const actorsEndpoint = "https://lanciweb.github.io/demo/api/actors/"
 
 export default function App() {
-  return 
+  const [actors, setActors] = useState ([]);
+  useEffect(() => {axios.get(actorsEndpoint)
+                        .then((res) => {console.table(res.data);
+    })}, 
+    []);
+  
+  return ( 
+    <div>
+      <ul>
+
+      </ul>
+    </div>
+  )
+
 }
 
